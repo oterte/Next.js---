148c691ca34aee9d62d19a7eb1a1bbeef926b026
oterte <97093479+oterte@users.noreply.github.com>
2023-05-01 20:56:47 +0900

    next.js의 Layout 활용하기

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+// 모든 페이지를 품을 수 있는 구조.
+export default function App({Component, pageProps}) {
+   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
+   return getLayout(<Component {...pageProps}/>)
+}
